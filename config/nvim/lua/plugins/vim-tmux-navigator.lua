ed0e8f352bc5110473f686e0875bd6bf3a3dd3ed
Tom Barlow <tomwbarlow@gmail.com>
2023-12-09 19:55:03 +0000

    Fix tmux

---
+++ config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,15 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  lazy = false,
+  cond = not is_windows,
+		-- stylua: ignore
+		keys = {
+			{ '<C-h>', '<cmd>TmuxNavigateLeft<CR>', mode = { 'n', 't' }, silent = true, desc = 'Jump to left pane' },
+			{ '<C-j>', '<cmd>TmuxNavigateDown<CR>', mode = { 'n', 't' }, silent = true, desc = 'Jump to lower pane' },
+			{ '<C-k>', '<cmd>TmuxNavigateUp<CR>', mode = { 'n', 't' }, silent = true, desc = 'Jump to upper pane' },
+			{ '<C-l>', '<cmd>TmuxNavigateRight<CR>', mode = { 'n', 't' }, silent = true, desc = 'Jump to right pane' },
+		},
+  init = function()
+    vim.g.tmux_navigator_no_mappings = true
+  end,
+}
