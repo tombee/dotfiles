967ba9f493ee82369af7d29415a47e678d1a5ade
Tom Barlow <tomwbarlow@gmail.com>
2023-12-09 19:56:26 +0000

    Add switch prev pane keybinding

--- config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -8,6 +8,7 @@ return {
 			{ '<C-j>', '<cmd>TmuxNavigateDown<CR>', mode = { 'n', 't' }, silent = true, desc = 'Jump to lower pane' },
 			{ '<C-k>', '<cmd>TmuxNavigateUp<CR>', mode = { 'n', 't' }, silent = true, desc = 'Jump to upper pane' },
 			{ '<C-l>', '<cmd>TmuxNavigateRight<CR>', mode = { 'n', 't' }, silent = true, desc = 'Jump to right pane' },
+			{ '<C-\\>', '<cmd>TmuxNavigateRight<CR>', mode = { 'n', 't' }, silent = true, desc = 'Jump to previous pane' },
 		},
   init = function()
     vim.g.tmux_navigator_no_mappings = true
