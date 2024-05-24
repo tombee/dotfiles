8019fe3a02872e07f1b699b132b16bfe8d5855fc
Tom Barlow <tbarlow@riotgames.com>
2024-05-24 20:31:09 +0100

    updates

--- config/nvim/plugin/keymaps.lua
+++ config/nvim/plugin/keymaps.lua
@@ -1,10 +1,5 @@
 local set = vim.keymap.set
 
-set("n", "<c-j>", "<c-w><c-j>")
-set("n", "<c-k>", "<c-w><c-k>")
-set("n", "<c-l>", "<c-w><c-l>")
-set("n", "<c-h>", "<c-w><c-h>")
-
 -- These mappings control the size of splits (height/width)
 set("n", "<M-,>", "<c-w>5<")
 set("n", "<M-.>", "<c-w>5>")
