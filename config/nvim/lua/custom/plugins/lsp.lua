2dd7db720952d32e95ccf67244636e0e7ace971a
Tom Barlow <tbarlow@riotgames.com>
2024-05-24 17:04:22 +0100

    Updates

--- config/nvim/lua/custom/plugins/lsp.lua
+++ config/nvim/lua/custom/plugins/lsp.lua
@@ -153,6 +153,7 @@ return {
       require("conform").setup {
         formatters_by_ft = {
           lua = { "stylua" },
+          python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
         },
       }
 
